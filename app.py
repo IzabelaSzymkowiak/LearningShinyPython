af9b4dbb318e215d67f9fc283905893fd536662c
Izabela <iza.szymkowiak0109@gmail.com>
2025-08-19 18:17:17 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,68 @@
+from shiny import App, render, ui, reactive
+import pandas as pd
+import matplotlib.ticker as mtick
+
+sales_data = pd.read_excel("C:/Users/izabe/Python-shiny/Adidas_US_Sales_Datasets.xlsx")
+sales_data = sales_data.iloc[3:, 1:]
+column_names = pd.Series(sales_data.iloc[0, ])
+for i in range(0, len(sales_data.columns)): 
+    sales_data = sales_data.rename(columns = {sales_data.columns[i] : column_names.iloc[i]})
+sales_data = sales_data.iloc[1:, ]
+
+sales_data = sales_data.astype({'Retailer' : 'str', 'Retailer ID' : 'int32', 'Invoice Date' : 'datetime64[ns]',
+     'State' : 'str', 'City' : 'str', 'Product' : 'str', 'Price per Unit' : 'int32', 'Units Sold' : 'int32',
+     'Total Sales' : 'int32', 'Operating Profit' : 'int32', 'Operating Margin' : 'float',
+     'Sales Method' : 'str'})
+
+app_ui = ui.page_fluid(
+    ui.panel_title("Adidas Sales"),
+    ui.layout_columns(
+        ui.input_select("retailer_select", "Choose a retailer:", sorted(sales_data['Retailer'].drop_duplicates()), selected = "Walmart"),
+        ui.input_date_range("daterange", "Date Range", start=min(sales_data['Invoice Date']), end=max(sales_data['Invoice Date']))
+    ),
+    ui.row(
+    ui.column(6, ui.output_plot("barchart")),
+    ui.column(6, ui.output_plot("piechart"))
+    ),
+    ui.output_table("data")
+)
+
+def server(input, output, session):
+    @reactive.Calc
+    def dataset():
+        selected_retailer = input.retailer_select() 
+        first_date = pd.to_datetime(input.daterange()[0])
+        last_date = pd.to_datetime(input.daterange()[1])
+        filtered_sales_dataset = sales_data[sales_data['Retailer'] == selected_retailer]
+        filtered_sales_dataset = filtered_sales_dataset[(filtered_sales_dataset['Invoice Date'] > first_date) &
+        (filtered_sales_dataset['Invoice Date'] < last_date)]
+        return filtered_sales_dataset
+
+    @reactive.Calc
+    def grouped_data():
+        sales_by_region = dataset().groupby(['Region']).agg(sales = ('Total Sales', 'sum'))
+        return sales_by_region
+
+    @render.plot
+    def barchart():
+        if not grouped_data().empty:
+            sales_by_region = grouped_data()
+            ax = sales_by_region.plot(kind='bar', title = 'Total Sales by Region', legend = False)
+            ax.yaxis.set_major_formatter(mtick.FuncFormatter(lambda x, _: f"{x/1000:.0f}K"))
+            for container in ax.containers:
+                ax.bar_label(container, labels=[f"{v/1000:.1f}K" for v in container.datavalues])
+            return ax 
+    
+    @render.plot
+    def piechart():
+        if not grouped_data().empty:
+            sales_by_region_perc = grouped_data()/grouped_data().sum()
+            ax = sales_by_region_perc.plot.pie(y='sales', legend = 'True', autopct='%1.1f%%' )
+            return ax
+
+    @render.table
+    def data():
+        return dataset().sort_values(by='Invoice Date').head()
+
+
+app = App(app_ui, server)
